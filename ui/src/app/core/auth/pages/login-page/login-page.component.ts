20a0da76836b4dd8c9a290773ba6edc95b3d4c80
felissedano <contact@felissedano.com>
2025-02-28 00:24:42 -0500

    ui: init login and register page

--- ui/src/app/core/auth/pages/login-page/login-page.component.ts
+++ ui/src/app/core/auth/pages/login-page/login-page.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthenticationService} from "../../authentication.service";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {AuthLayoutComponent} from "../../../layouts/auth-layout/auth-layout.component";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatFormField, MatInput, MatSuffix} from "@angular/material/input";
+import {FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {MatDateRangeInput} from "@angular/material/datepicker";
+import {MatIcon} from "@angular/material/icon";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
-  standalone: true,
-  imports: [],
+  imports: [MatSlideToggleModule, AuthLayoutComponent, MatButton, MatInput, ReactiveFormsModule, MatFormField, MatDateRangeInput, MatIcon, MatIconButton, MatSuffix, RouterLink],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
 
+
+  hidePwd = true;
+  toggleHidePwd() {
+    this.hidePwd = !this.hidePwd;
+  }
+  getInputType() {
+    return this.hidePwd ? 'password' : 'text';
+  }
+  getVisibilityIcon() {
+    return this.hidePwd ? 'visibility_off' : 'visibility';
+  }
 }
--- ui/src/app/core/auth/pages/register-page/register-page.component.ts
+++ ui/src/app/core/auth/pages/register-page/register-page.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-register-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './register-page.component.html',
-  styleUrl: './register-page.component.scss'
+    selector: 'app-register-page',
+    imports: [],
+    templateUrl: './register-page.component.html',
+    styleUrl: './register-page.component.scss'
 })
 export class RegisterPageComponent {
 
