83d7a60cf4c72f286d20fd99210d5b3a00bb714c
felissedano <contact@felissedano.com>
2025-03-04 18:28:06 -0500

    server: migrate deprecated security config to new one

--- server/src/main/java/com/felissedano/dailyreflect/auth/WebSecurityConfiguration.java
+++ server/src/main/java/com/felissedano/dailyreflect/auth/WebSecurityConfiguration.java
@@ -3,14 +3,16 @@ package com.felissedano.dailyreflect.auth;
 import com.felissedano.dailyreflect.auth.services.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.config.Customizer;
 @Configuration
 public class WebSecurityConfiguration {
 
@@ -31,13 +33,27 @@ public class WebSecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable().authorizeHttpRequests((reqMatcherRegistry) ->
+        httpSecurity.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests((reqMatcherRegistry) ->
                 reqMatcherRegistry
                         .requestMatchers("/").permitAll()
                         .requestMatchers("/api/test/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
-        );
+        ).logout(httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer
+                .logoutUrl("/api/auth/logout")
+                .logoutSuccessHandler((request, response, authentication) -> {
+                    response.setStatus(HttpStatus.OK.value());
+                    response.getWriter().write("Logout successful");
+                })
+                .deleteCookies("JSESSIONID")
+                .invalidateHttpSession(true)
+                .permitAll()
+                )
+                .exceptionHandling(ex -> ex.authenticationEntryPoint((request, response, authException) -> {
+                    response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                    response.getWriter().write("Unauthorized");
+                }))
+                .sessionManagement(session -> {session.maximumSessions(2);});
         return httpSecurity.build();
     }
 
