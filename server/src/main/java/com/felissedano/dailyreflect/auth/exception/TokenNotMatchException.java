1ed1cc976ac4d7971b94403d18604eb41f7895cb
felissedano <contact@felissedano.com>
2025-03-08 17:07:01 -0500

    server: add i18n error message feature

--- server/src/main/java/com/felissedano/dailyreflect/auth/exception/TokenNotMatchException.java
+++
@@ -1,7 +0,0 @@
-package com.felissedano.dailyreflect.auth.exception;
-
-public class TokenNotMatchException extends RuntimeException {
-    public TokenNotMatchException(String message) {
-        super(message);
-    }
-}
