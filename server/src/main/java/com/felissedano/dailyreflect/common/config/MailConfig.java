bcbb87fb4902c22c8d84ae7162b8afc00f14f828
felissedano <contact@felissedano.com>
2025-03-11 14:16:31 -0400

    generali+server: add maildev container for email testing during development; create mock JavaMailSender bean for testing without mail server

--- server/src/main/java/com/felissedano/dailyreflect/common/config/MailConfig.java
+++ server/src/main/java/com/felissedano/dailyreflect/common/config/MailConfig.java
@@ -1,45 +1,65 @@
 package com.felissedano.dailyreflect.common.config;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.Session;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 @Configuration
-//@ConfigurationProperties(prefix = "spring.mail")
-//@ConfigurationProperties
 public class MailConfig {
 
 
-
-    @Value("${spring.mail.protocol}")
-    String protocol;
-
-    @Value("${spring.mail.port}")
-    int port;
-
-    @Value("${spring.mail.host}")
-    String host;
-
-    @Value("${spring.mail.username}")
-    String username;
-
-    @Value("${spring.mail.password}")
-    String password;
-
     @Bean
+    @Profile("mock")
     JavaMailSender javaMailSender() {
-        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        return new MockMailSender();
+    }
 
-        javaMailSender.setHost(this.host);
-        javaMailSender.setPort(this.port);
-        javaMailSender.setProtocol(this.protocol);
-        javaMailSender.setUsername(this.username);
-        javaMailSender.setPassword(this.password);
+    private static class MockMailSender implements JavaMailSender {
 
-        return javaMailSender;
+        @Override
+        public MimeMessage createMimeMessage() {
+            return new MimeMessage((Session) null);
+        }
+
+
+        @Override
+        public MimeMessage createMimeMessage(InputStream contentStream) throws MailException {
+            return new MimeMessage((Session) null);
+        }
+
+        @Override
+        public void send(MimeMessage... mimeMessages) throws MailException {
+            for (MimeMessage mimeMessage : mimeMessages) {
+                try {
+                    System.out.println(STR."\nMimeMessage email -> \nSubject: \{mimeMessage.getSubject()} \nContent : \{mimeMessage.getContent()}\n");
+                } catch (MessagingException e) {
+                    System.out.println("Some errors happened when try to print mimeMessage");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        @Override
+        public void send(SimpleMailMessage... simpleMessages) throws MailException {
+            for (SimpleMailMessage simpleMessage : simpleMessages) {
+                System.out.println(STR."\nSimpleMailMessage email -> \nSubject: \{simpleMessage.getSubject()} \nContent: \{simpleMessage.getText()}\n");
+            }
+        }
     }
 
 
