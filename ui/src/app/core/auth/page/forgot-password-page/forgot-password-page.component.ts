81c4f92e4dfb5e8a2cb5c2ed5cc5db3d1d8a3071
felissedano <contact@felissedano.com>
2025-03-26 21:29:47 -0400

    ui: create forgot password request page

---
+++ ui/src/app/core/auth/page/forgot-password-page/forgot-password-page.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import {AuthLayoutComponent} from "../../../layout/auth-layout/auth-layout.component";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {NgIf} from "@angular/common";
+import {MatButton} from "@angular/material/button";
+import {AuthService} from "../../auth.service";
+
+@Component({
+  selector: 'app-forgot-password-page',
+  imports: [
+    AuthLayoutComponent,
+    ReactiveFormsModule,
+    MatLabel,
+    MatInput,
+    MatError,
+    NgIf,
+    MatFormField,
+    MatButton
+  ],
+  templateUrl: './forgot-password-page.component.html',
+  styleUrl: './forgot-password-page.component.scss'
+})
+export class ForgotPasswordPageComponent {
+
+  constructor(private authService: AuthService) {
+  }
+
+  actionState: 'not-sent' | 'sent' = "not-sent";
+
+  errorMsg: string | null = null;
+
+  forgotPasswordForm = new FormGroup({
+    email: new FormControl("", [Validators.required, Validators.email])
+  });
+
+  onSubmit(){
+    if (! this.forgotPasswordForm.valid) {
+      return
+    }
+    this.authService.sendPasswordResetLink(this.forgotPasswordForm.value.email as string).subscribe({
+      next: value => this.actionState = "sent",
+      error: err => { console.error(err); this.errorMsg = err.error.detail;}
+      }
+    )
+  }
+
+}
