0ea301a9f1c505ba7f2079fa74ddceeb81f352a5
felissedano <contact@felissedano.com>
2025-09-27 21:19:01 -0400

    chore: update flake and angular frontend

--- flake.nix
+++ flake.nix
@@ -1,18 +1,17 @@
 {
   description = "A nix flake base dev environment.";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
   outputs = { self, nixpkgs }:
-    let 
+    let
       supportedSystems =
         [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f:
         nixpkgs.lib.genAttrs supportedSystems
         (system: f { pkgs = import nixpkgs { inherit system; }; });
     in {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = import ./shell.nix { inherit pkgs; };      
-      });
+      devShells = forEachSupportedSystem
+        ({ pkgs }: { default = import ./shell.nix { inherit pkgs; }; });
     };
 }
