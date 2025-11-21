e178adae26c7eab2716248374d0db5b0345f69b5
felissedano <contact@felissedano.com>
2025-11-20 23:54:06 -0500

    chore: add localization to date

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -2,21 +2,22 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { environment } from '../environments/environment';
+import localFr from '@angular/common/locales/fr-CA';
+import localEn from '@angular/common/locales/en-CA';
+import { DatePipe, registerLocaleData } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  providers: [DatePipe]
 })
 export class AppComponent {
   title = environment.appName;
 
   constructor(private translateService: TranslateService) {
-    this.translateService.addLangs(['en', 'fr']);
-    this.translateService.setDefaultLang('en');
-    this.translateService.use(
-      translateService.getBrowserLang() === 'fr' ? 'fr' : 'en',
-    );
+    registerLocaleData(localFr, 'fr');
+    registerLocaleData(localEn, 'en');
   }
 }
